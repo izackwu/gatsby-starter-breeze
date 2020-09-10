8616dbfd7f6f5ce56e658ed293b98d1c075362bc
Keith Null <keith1126@126.com>
2020-09-10 21:48:53 +0800

    Switch to scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
