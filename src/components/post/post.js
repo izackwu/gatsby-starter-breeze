340055ac191e58947fc8c37fdd87bef695039858
Keith Null <keith1126@126.com>
2020-09-13 21:20:11 +0800

    Apply styles to post pages

---
+++ src/components/post/post.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { Link } from "gatsby"
+import { FaRegCalendarAlt, FaTags } from "react-icons/fa"
+import styles from "./post.module.scss"
+
+const _ = require("lodash")
+
+const Post = ({ post }) => {
+  return (
+    <article>
+      <header className={styles["header"]}>
+        {post.frontmatter.image && (
+          <div
+            className={styles["header__image"]}
+            style={{ backgroundImage: `url(${post.frontmatter.image})` }}
+          ></div>
+        )}
+        <div className={styles["header__info"]}>
+          <h1 className={styles["header__info__title"]}>
+            {post.frontmatter.title}
+          </h1>
+          <span className={styles["header__info__date"]}>
+            <FaRegCalendarAlt className={styles["icon"]} />
+            {post.fields.date}
+          </span>
+          {post.frontmatter.tags && (
+            <span className={styles["header__info__tags"]}>
+              <FaTags className={styles["icon"]} />
+              <ol>
+                {post.frontmatter.tags.map(tag => (
+                  <li>
+                    <Link to={`/tags/${_.kebabCase(tag)}/`}>{tag}</Link>
+                  </li>
+                ))}
+              </ol>
+            </span>
+          )}
+        </div>
+      </header>
+      <section
+        className={styles["content"]}
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+    </article>
+  )
+}
+
+export default Post
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,13 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Sidebar from "../components/sidebar"
 import Main from "../components/main"
 import Pagination from "../components/pagination"
+import Post from "../components/post"
 
-const _ = require("lodash")
-
-const BlogPostTemplate = ({ data, pageContext, location }) => {
+const BlogPostTemplate = ({ data, pageContext }) => {
   const post = data.markdownRemark
   const { previous, next } = pageContext
 
@@ -20,59 +19,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
     >
       <Sidebar />
       <Main>
-        <article>
-          <header>
-            <h1
-              style={{
-                marginBottom: 0,
-              }}
-            >
-              {post.frontmatter.title}
-            </h1>
-            <ul
-              style={{
-                display: `inline-flex`,
-                justifyContent: `flex-start`,
-                flexWrap: `wrap`,
-                listStyle: `none`,
-                margin: `0 0 0 10px`,
-              }}
-            >
-              {post.frontmatter.tags &&
-                post.frontmatter.tags.map(tag => {
-                  return (
-                    <li>
-                      <Link
-                        to={`/tags/${_.kebabCase(tag)}/`}
-                        style={{
-                          marginRight: `15px`,
-                          padding: `0 1.5em`,
-                          border: `1px solid #e6e6e6`,
-                          borderRadius: `1.25em`,
-                          textDecoration: `none`,
-                          display: `inline-block`,
-                          boxShadow: `none`,
-                        }}
-                      >
-                        {tag}
-                      </Link>
-                    </li>
-                  )
-                })}
-            </ul>
-            <p
-              style={{
-                display: `block`,
-              }}
-            >
-              {post.fields.date}
-            </p>
-          </header>
-          <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr style={{}} />
-          <footer></footer>
-        </article>
-
+        <Post post={post} />
         <Pagination
           prevLink={previous && previous.fields.slug}
           prevText={previous && "← " + previous.frontmatter.title}
