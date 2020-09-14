633323a7173627b18b1161fdee4b50344d5e096c
Keith Null <keith1126@126.com>
2020-09-14 18:58:16 +0800

    Add compact mode for post list component

--- src/components/postlist/postlist.js
+++ src/components/postlist/postlist.js
@@ -2,25 +2,43 @@ import React from "react"
 import styles from "./postlist.module.scss"
 import { Link } from "gatsby"
 
-const PostList = ({ posts }) => {
+const PostList = ({ posts, compact }) => {
   console.log(styles)
   return (
     <div className={styles["postlist"]}>
       {posts.map(({ node }) => (
         <div className={styles["postlist__item_wrapper"]}>
           <Link to={node.fields.slug} className={styles["item"]}>
-            <div className={styles["item__info"]}>
-              <h2 className={styles["item__info__title"]}>
+            <div
+              className={
+                compact ? styles["item__info_compact"] : styles["item__info"]
+              }
+            >
+              <h2
+                className={
+                  compact
+                    ? styles["item__info_compact__title"]
+                    : styles["item__info__title"]
+                }
+              >
                 {node.frontmatter.title}
               </h2>
-              <div className={styles["item__info__description"]}>
-                {node.frontmatter.description || node.excerpt}
-              </div>
-              <div className={styles["item__info__date"]}>
+              {!compact && (
+                <div className={styles["item__info__description"]}>
+                  {node.frontmatter.description || node.excerpt}
+                </div>
+              )}
+              <div
+                className={
+                  compact
+                    ? styles["item__info_compact__date"]
+                    : styles["item__info__date"]
+                }
+              >
                 {node.fields.date}
               </div>
             </div>
-            {node.frontmatter.image && (
+            {!compact && node.frontmatter.image && (
               <div
                 className={styles["item__image"]}
                 style={{ backgroundImage: `url(${node.frontmatter.image})` }}
