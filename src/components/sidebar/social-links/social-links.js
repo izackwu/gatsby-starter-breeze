3258b301779ab699b65d77fbb58314b25f977291
Keith Null <keith1126@126.com>
2020-09-18 22:36:40 +0800

    Add icons for social links in sidebar

---
+++ src/components/sidebar/social-links/social-links.js
@@ -0,0 +1,32 @@
+import { getIcon, getLink } from "./utils"
+
+import React from "react"
+import styles from "./social-links.module.scss"
+
+const SocialLinks = ({ social }) => {
+  return (
+    <div className={styles["social"]}>
+      <ul className={styles["social__list"]}>
+        {social &&
+          Object.entries(social).map(entry => {
+            const [sitename, username] = entry
+            return (
+              username && (
+                <li key={sitename} className={styles["social__list__item"]}>
+                  <a
+                    href={getLink(sitename, username)}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {getIcon(sitename)}
+                  </a>
+                </li>
+              )
+            )
+          })}
+      </ul>
+    </div>
+  )
+}
+
+export default SocialLinks
---
+++ src/components/sidebar/social-links/utils.js
@@ -0,0 +1,69 @@
+import {
+  FaFacebookF,
+  FaGithub,
+  FaLinkedinIn,
+  FaRss,
+  FaTelegramPlane,
+  FaTwitter,
+  FaYoutube,
+  ImShare2,
+  IoMdMail,
+  RiDoubanFill,
+  RiInstagramFill,
+} from "react-icons/all"
+
+import React from "react"
+
+export const getIcon = sitename => {
+  switch (sitename) {
+    case "douban":
+      return <RiDoubanFill />
+    case "email":
+      return <IoMdMail />
+    case "facebook":
+      return <FaFacebookF />
+    case "github":
+      return <FaGithub />
+    case "instagram":
+      return <RiInstagramFill />
+    case "linkedin":
+      return <FaLinkedinIn />
+    case "rss":
+      return <FaRss />
+    case "telegram":
+      return <FaTelegramPlane />
+    case "twitter":
+      return <FaTwitter />
+    case "youtube":
+      return <FaYoutube />
+    default:
+      return <ImShare2 />
+  }
+}
+
+export const getLink = (sitename, username) => {
+  switch (sitename) {
+    case "douban":
+      return `https://www.douban.com/people/${username}`
+    case "email":
+      return `mailto:${username}`
+    case "facebook":
+      return `https://www.facebook.com/${username}`
+    case "github":
+      return `https://github.com/${username}`
+    case "instagram":
+      return `https://www.instagram.com/${username}`
+    case "linkedin":
+      return `https://www.linkedin.com/in/${username}`
+    case "rss":
+      return username
+    case "telegram":
+      return `https://t.me/${username}`
+    case "twitter":
+      return `https://www.twitter.com/${username}`
+    case "youtube":
+      return `https://www.youtube.com/channel/${username}`
+    default:
+      return username
+  }
+}
